4d142bdcf3431b78d53df2611262bab465f7e2cd
Nikolaos Sarris <int02051@uoi.gr>
2021-04-21 19:09:14 +0300

    a new file hase been added to my repository

--- ergasia_file_counter.py
+++ ergasia_file_counter.py
@@ -3,7 +3,10 @@ sys.path.append('..')
 import file_walk as fw
 import re
 import os
-import termcolor as tm
+
+from time import time
+
+
 def file_parser():
     print(os.getcwd())
     allfiles = []
@@ -18,13 +21,14 @@ except:
     return list([])
 return allfiles
 
-
+#1!!!!!!!!!!!!!!!!!!!!!!!!!
 def filesbycategory():
+
     counter = {'cpp': 0, 'hpp': 0, 'h': 0, 'c': 0}
     starttime = time()
     for x in files:
         if re.match('.+\.cpp$', x):
-          # if x.endswith('.cpp'):
+            # if x.endswith('.cpp'):
             counter['cpp'] += 1
         elif x.endswith('.hpp'):
             counter['hpp'] += 1
@@ -38,6 +42,18 @@ def filesbycategory():
     endtime = time()
     print('Lapsed Time:' + str(endtime - starttime) + ' s')
 
+#2!!!!!!!!!!!!!!!!!!!!!!!!!!
+starttime=time()
+    linecounter=0
+    for i in files:
+        with open(i,'r',encoding="utf-8",errors='ignore') as f:
+            for j in f:
+                if len(j.strip())!=0:
+                    linecounter+=1
+    print('Lines of code:'+str(linecounter))
+    print('Elapsed Time for finding all lines of code:'+str(time()-starttime)+'\'s')
+
+#3!!!!!!!!!!!!!!!!!!!!!!!
 def symbols_letters_digid():
     parser={'characters':0,'digits':0,'symbols':0}
     patternC=re.compile('[A-Za-z]')
@@ -56,52 +72,41 @@ def symbols_letters_digid():
                 parser['symbols'] +=len(k)-(chars+digits)
     end_time=time()
     for x in parser:
-        tm.cprint(f'{x}-->{parser[x]}','green')
-    tm.cprint('=='*30,'red')
-    tm.cprint(f'lapsed time:{end_time-start_time} \ 's','red')
+        print(f'{x}-->{parser[x]}')
+    print('=='*30)
+    print(f'lapsed time:{end_time-start_time} \ 's')
 symbols_letters_digid()
 
-starttime=time()
-    linecounter=0
-    for i in files:
-        with open(i,'r',encoding="utf-8",errors='ignore') as f:
-            for j in f:
-                if len(j.strip())!=0:
-                    linecounter+=1
-    tm.cprint('Lines of code:'+str(linecounter),'red')
-    tm.cprint('Elapsed Time for finding all lines of code:'+str(time()-starttime)+'\'s','red')
+#4!!!!!!!!!!!!!!!!!!!!!!!!!
 
-def equality_statements():
     def equality_statements():
-    counter=0
-    fls=[]
-    pattern=re.compile('\s+if\s*\(\s*.+\s*==\s*.+\)|^if\s*\(.+==.+\)|.*if\s*\(\s*\w+\s*\)')
-    patternB=re.compile("\s*if\s*\(\s*\w+\s*\)")
-    #|if\s*\(\s*(\w|\d)+\s*\)
-    #if\s*\(\s*(\w|\d)+\s*\)-->if(k) αν k είναι boolean μεταβλητή η ένας αριθμός
-    for x in files:
-        with open(x,'r',encoding='utf-8',errors='ignore') as f:
-            #counter+=len([m for m in f if(r.match('.*if(.+==.+).*',m))])
-            for k in f:
-                print(patternB.findall(k))
-                fls+=pattern.findall(k)
-                fls+=patternB.findall(k)
-    tm.cprint('\t If equality Statements found','blue')
-    tm.cprint('==='*30,'red')
-    id=1
-    for k in fls:
-        k=k.replace(' ','').replace('\t','')
-        #k=k.strip()
-        tm.cprint(str(id)+'.'+str(k),'green')
-        id+=1
-    print('\n\n')
-    return len(fls)
+        def equality_statements():
+            counter = 0
 
+        fls = []
+        pattern = re.compile('\s+if\s*\(\s*.+\s*==\s*.+\)|^if\s*\(.+==.+\)|.*if\s*\(\s*\w+\s*\)')
+        patternB = re.compile("\s*if\s*\(\s*\w+\s*\)")
+        # |if\s*\(\s*(\w|\d)+\s*\)
+        # if\s*\(\s*(\w|\d)+\s*\)-->if(k) αν k είναι boolean μεταβλητή η ένας αριθμός
+        for x in files:
+            with open(x, 'r', encoding='utf-8', errors='ignore') as f:
+                # counter+=len([m for m in f if(r.match('.*if(.+==.+).*',m))])
+                for k in f:
+                    print(patternB.findall(k))
+                    fls += pattern.findall(k)
+                    fls += patternB.findall(k)
+        print('\t If equality Statements found')
+        print('===' * 30)
+        id = 1
+        for k in fls:
+            k = k.replace(' ', '').replace('\t', '')
+            # k=k.strip()
+            tm.cprint(str(id) + '.' + str(k), 'green')
+            id += 1
+        print('\n\n')
+        return len(fls)
 
-#Χρήση custom module file_walk:[https://github.com/vasnastos/Assignment_AGP/blob/master/file_walk.py]
-#Απαραίτητος ο φάκελος oop-master
-files=fw.file_parser()
-
+#5!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 def over_12_characters():
     found=0
     print(len(files))
@@ -119,47 +124,24 @@ def over_12_characters():
                    print('')
     print(f'for loops with over 12 characters:{found}')
 
-#Test σε αρχείο sample.cpp
-def tester():
-    found=0
-    pattern=re.compile('for\s*\(.+;.+;.+\)|for\s*\(.+[:].+\)')
-    with open("sample.cpp",'r',encoding='utf-8') as L:
-        for x in L:
-            for l in pattern.findall(x):
-                   tm.cprint(l,'red')
-                   current=l[int(l.find('for('))+4:len(l)-1]
-                   if len(current.replace(' ',''))>=12:
-                         tm.cprint(current,'green')
-                         found+=1
-                   print('')
-    print(f'for loops with over 12 characters:{found}')
+#6!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+def commonvars():
+    vars = {}
+    # Τρια Μοτίβα μεταβλητών
+    # int a;-->int\s+\w+;
+    # int a=10;-->int\s+\w+\s*([=]\s*\w+\s*)+;
+    # int a,b,c;-->int\s+\w+\s*([,]\s*\w+\s*)*;
+    # Μη αποδεκτό-->const int a=10;
 
-#Περίπτωση με εύρεση kαι inline εντολών for
-#split based for
-def tester1():
-    found=0
-    pattern=re.compile('\(.+;.+;.+\)|\(.+[:].+\)')
-    with open('sample.cpp','r') as f:
-        for x in f:
-            data=[]
-            if 'for' in x:
-                data=x.split('for')
-            if len(data)==0:
-                continue
-            for k in data:
-               if pattern.match(k):
-                   tm.cprint(k,'red')
-                   for m in pattern.findall(k):
-                       fixed=m[m.find('(')+1:len(m)-1]
-                       fixed=fixed.strip().replace(' ','')
-                       if len(fixed)>=12:
-                           tm.cprint(len(fixed),'yellow')
-                           tm.cprint(fixed,'green')
-                           found+=1
-               tm.cprint('============================================================','white')
-        tm.cprint(f'Matches Found:{found}','blue')
+    # (int\s+\w+\s*([,]\s*\w+\s*)*[;]|\s*int\s+\w+\s*([=]\s*\w+\s*)+[;])
+    patternf = re.compile('(int\s+\w+\s*([,]\s*\w+\s*)*[;]|\s*int\s+\w+\s*([=]\s*\w+\s*)+[;])')
+    constpattern = re.compile('(const\s+int\s*\w+\s*([=]\s*\w+\s*)+[;])')
+    for x in files:
+        with open(x, 'r', encoding='utf-8', errors='ignore') as L:
+            for k in L:
+                cleanconst = k
+                for l in constpattern.findall(k):
+                    cleanconst = k.replace(l[0], '')
+                for t in patternf.findall(cleanconst):
+                    print('Variable:' + str(t))
 
-over_12_characters()
-#tester()
-#tester1()
-#!!!
